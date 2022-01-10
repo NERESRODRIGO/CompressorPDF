a9b6d36c6d5e0df7568f2fecaa7cdfcbf44a031f
Rodrigo Neres <neres.rodrigo@hotmail.com>
2022-01-10 13:51:12 -0300

    Update and rename CompressorPDF1.py to CompressorPDF-SemDiretório.py

--- CompressorPDF-SemDiretório.py
+++ CompressorPDF-SemDiretório.py
@@ -8,12 +8,6 @@ for root, dirs, files in os.walk("C:\comp"): #Diretório onde será realizada a 
             filename = os.path.join(root, file)
             print (filename)
             arg1= '-sOutputFile=' + "c" +  file #Adiciona a letra c ao nome de cada arquivo gerado
-            #Para alterar a qualidade/compressão do arquivo o parâmetro -dPDFSETTINGS= pode receber os seguintes valores
-                #/screen - baixa resolução
-                #/ebook - média resolução
-                #/printer - saída similar ao Acrobat Distiller "Print Optimized"
-                #/prepress - saída similar ao Acrobat Distiller "Prepress Optimized"
-                #Outras configurações podem ser encontradas em https://www.ghostscript.com/doc/9.54.0/VectorDevices.htm
             p = subprocess.Popen(['C:/Program Files/gs/gs9.55.0/bin/gswin64c.exe',
                                   '-sDEVICE=pdfwrite', 
                                   '-dCompatibilityLevel=1.4', 
